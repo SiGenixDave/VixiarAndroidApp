d053c1f25022a3bf652e24fc4f7963093ff8f685
Greg Yurko <gyurko@sigenix.com>
2017-10-11 10:29:46 -0400

    Read the version number from build.gradle and display it at the bottom.

--- app/src/main/java/com/sigenix/indicor/MainActivity.java
+++ app/src/main/java/com/sigenix/indicor/MainActivity.java
@@ -2,6 +2,7 @@ package com.sigenix.indicor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +12,12 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // put the version number into the bottom status bar
+        String nameAndVersion = getString(R.string.application_name) +
+                " V" + BuildConfig.VERSION_NAME;
+
+        TextView versionText = (TextView)findViewById(R.id.textViewVersion);
+        versionText.setText(nameAndVersion);
     }
 }
