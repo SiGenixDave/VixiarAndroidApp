fdad7dc021e9a32dddd781e3737b41c075feaa78
Greg Yurko <gyurko@sigenix.com>
2017-10-18 09:47:45 -0400

    Backup commit...making progress on patient entry activity

--- app/src/main/java/com/sigenix/indicor/PatInfoActivity.java
+++ app/src/main/java/com/sigenix/indicor/PatInfoActivity.java
@@ -15,11 +15,13 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 
 public class PatInfoActivity extends Activity
 {
@@ -33,31 +35,22 @@ public class PatInfoActivity extends Activity
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
     private EditText txtPatientID;
+    private EditText txtDOB;
+
+    private NumberPicker npDOBMonth;
+    private NumberPicker npDOBDay;
+    private NumberPicker npDOBYear;
+
+    private String[] monthString;
+    private String[] genderString;
 
     @TargetApi(Build.VERSION_CODES.M) // This is required for Android 6.0 (Marshmallow) to work
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_pat_info);
-
-        txtPatientID = (EditText) findViewById(R.id.txtPatientID);
-        txtPatientID.setOnFocusChangeListener(new View.OnFocusChangeListener()
-        {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus)
-            {
-                if (hasFocus)
-                {
-                    txtPatientID.setTextColor(getResources().getColor(R.color.colorPatientEntryHighlightedValue));
-                }
-                else
-                {
-                    txtPatientID.setTextColor(getResources().getColor(R.color.colorPatientEntryNormalValue));
-                }
-            }
-        });
+        initializeControls();
 
 
         // FULL SCREEN (add if FS is desired)
@@ -198,5 +191,88 @@ public class PatInfoActivity extends Activity
         }
     }
 
+    private void initializeControls()
+    {
+        txtPatientID = (EditText) findViewById(R.id.txtPatientID);
+        txtPatientID.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (hasFocus)
+                {
+                    txtPatientID.setTextColor(ContextCompat.getColor(PatInfoActivity.this, R.color.colorPatientEntryHighlightedValue));
+                }
+                else
+                {
+                    txtPatientID.setTextColor(ContextCompat.getColor(PatInfoActivity.this, R.color.colorPatientEntryNormalValue));
+                }
+            }
+        });
+
+        txtDOB = (EditText) findViewById(R.id.txtDOB);
+        txtDOB.setShowSoftInputOnFocus(false);
+        txtDOB.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (hasFocus)
+                {
+                    txtDOB.setTextColor(getResources().getColor(R.color.colorPatientEntryHighlightedValue));
+                    npDOBDay.setVisibility(View.VISIBLE);
+                    npDOBMonth.setVisibility(View.VISIBLE);
+                    npDOBYear.setVisibility(View.VISIBLE);
+
+                }
+                else
+                {
+                    txtDOB.setTextColor(getResources().getColor(R.color.colorPatientEntryNormalValue));
+                    npDOBDay.setVisibility(View.GONE);
+                    npDOBMonth.setVisibility(View.GONE);
+                    npDOBYear.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        monthString = getResources().getStringArray(R.array.months_array);
+
+        NumberPicker.OnValueChangeListener dobChangeListener = new NumberPicker.OnValueChangeListener()
+        {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal)
+            {
+                int nMonth = npDOBMonth.getValue();
+                int nDay = npDOBDay.getValue();
+                int nYear = npDOBYear.getValue();
+                txtDOB.setText(monthString[nMonth] + " " + String.valueOf(nDay) + " " + String.valueOf(nYear));
+            }
+        };
+
+        npDOBDay = (NumberPicker) findViewById(R.id.npDOBDay);
+        npDOBDay.setVisibility(View.GONE);
+        npDOBDay.setMinValue(1);
+        npDOBDay.setMaxValue(31);
+        npDOBDay.setOnValueChangedListener(dobChangeListener);
+
+        npDOBMonth = (NumberPicker) findViewById(R.id.npDOBMonth);
+        npDOBMonth.setVisibility(View.GONE);
+        npDOBMonth.setMinValue(0);
+        npDOBMonth.setMaxValue(monthString.length-1);
+        npDOBMonth.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                return monthString[value];
+            }
+        });
+        npDOBMonth.setOnValueChangedListener(dobChangeListener);
+
+        npDOBYear = (NumberPicker) findViewById(R.id.npDOBYear);
+        npDOBYear.setVisibility(View.GONE);
+        npDOBYear.setMinValue(1930);
+        npDOBYear.setMaxValue(2017);
+        npDOBYear.setValue(1960);
+        npDOBYear.setOnValueChangedListener(dobChangeListener);
+    }
 }
 
