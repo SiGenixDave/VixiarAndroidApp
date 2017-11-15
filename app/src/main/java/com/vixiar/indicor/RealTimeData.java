9a1966705d6825d496759e0c6a9fa30cd9838855
Greg Yurko <gyurko@sigenix.com>
2017-11-15 18:45:44 -0500

    Working on scaling the pressure data.

--- app/src/main/java/com/vixiar/indicor/RealTimeData.java
+++ app/src/main/java/com/vixiar/indicor/RealTimeData.java
@@ -8,24 +8,33 @@ import java.util.ArrayList;
 
 /* scaling info
 pressure sensor Vout = VS*[(0.1533*P) + 0.053]  ...  Vs = 5.0V, P is pressure in kPa
-1 mmHg = 0.133322368 kPa
+1 kPa = 0.133322368 mmHg
 circuit board 0V at pressure sensor = 3V at A/D. 5V at pressure sensor = 0V at A/D
 A/D 3.3V = 2048 counts
+doing the math, pressure(mmHg)=(counts-1763.1)/-38.052
 
+*/
 public class RealTimeData
 {
     private ArrayList<PPG_PressureData> data;
-    public void AppendData(byte [] data)
+    public void AppendData(byte [] new_data)
     {
         // extract the data...the first byte is the sequence number
         // followed by two bytes of PPG then pressure repetitively
         double pressure_value = 0.0;
+        int pressure_counts = 0;
         int ppg_value = 0;
-        for (int i = 1; i < data.length; i += 4)
+        for (int i = 1; i < new_data.length; i += 4)
         {
-            ppg_value = (256 * (int)(data[i] & 0xFF)) + (data[i+1] & 0xFF);
-
+            ppg_value = (256 * (int)(new_data[i] & 0xFF)) + (new_data[i+1] & 0xFF);
+            pressure_counts = (256 * (int)(new_data[i+2] & 0xFF)) + (new_data[i+3] & 0xFF);
+            pressure_value = ((double)pressure_counts-1763.1)/-38.052;
+            PPG_PressureData pd = new PPG_PressureData(ppg_value, pressure_value);
+            data.add(pd);
         }
-
     };
+    public ArrayList<PPG_PressureData> GetData()
+    {
+        return data;
+    }
 }
