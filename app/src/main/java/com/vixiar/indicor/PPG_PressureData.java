cd1ae3aad5fc3bd90a5cec79a7ff7411df7ab455
Greg Yurko <gyurko@sigenix.com>
2017-11-15 18:45:14 -0500

    Working on scaling the pressure data.

--- app/src/main/java/com/vixiar/indicor/PPG_PressureData.java
+++ app/src/main/java/com/vixiar/indicor/PPG_PressureData.java
@@ -6,6 +6,11 @@ package com.vixiar.indicor;
 
 public class PPG_PressureData
 {
+    public PPG_PressureData(int PPG, double pressure)
+    {
+        PPG = PPG;
+        pressure = pressure;
+    }
     private double pressure;
     private int PPG;
 }
