4d94320120c5c2beafd5dc942d246390a93bd42d
Greg Yurko <gyurko@sigenix.com>
2017-12-27 10:53:37 -0500

    Start the upload service from the application.

--- app/src/main/java/com/vixiar/indicor/Application/NavigatorApplication.java
+++ app/src/main/java/com/vixiar/indicor/Application/NavigatorApplication.java
@@ -3,6 +3,8 @@ package com.vixiar.indicor.Application;
 import android.app.Application;
 import android.content.Context;
 
+import com.vixiar.indicor.Upload_Interface.UploadServiceInterface;
+
 /**
  * Created by gyurk on 12/19/2017.
  */
@@ -14,6 +16,8 @@ public class NavigatorApplication extends Application
     public void onCreate() {
         super.onCreate();
         m_context = getApplicationContext();
+
+        UploadServiceInterface.getInstance().initialize(this);
     }
 
     public static Context getAppContext() {
