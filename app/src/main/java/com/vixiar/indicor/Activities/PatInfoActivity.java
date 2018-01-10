cf9c564639aa3938005c79a96e100084aa8b8fd1
Greg Yurko <gyurko@sigenix.com>
2018-01-10 09:02:03 -0500

    Re-enabled the check that all of the data is entered on the PI screen before allowing the user to proceed.

--- app/src/main/java/com/vixiar/indicor/Activities/PatInfoActivity.java
+++ app/src/main/java/com/vixiar/indicor/Activities/PatInfoActivity.java
@@ -34,7 +34,7 @@ public class PatInfoActivity extends Activity implements CustomDialogInterface, 
 {
     // TAG is used for informational messages
     private final static String TAG = PatInfoActivity.class.getSimpleName();
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     private EditText txtPatientID;
     private EditText txtAge;
