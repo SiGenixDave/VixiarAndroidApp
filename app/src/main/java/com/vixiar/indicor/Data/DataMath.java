bfe37ec13da4d04e379cc7efeb6be09efde8de2d
Greg Yurko <gyurko@sigenix.com>
2018-09-07 12:49:42 -0400

    Moved mean and stdev functions to their own class.

---
+++ app/src/main/java/com/vixiar/indicor/Data/DataMath.java
@@ -0,0 +1,35 @@
+package com.vixiar.indicor.Data;
+
+import java.util.ArrayList;
+
+public class DataMath
+{
+    private static final DataMath ourInstance = new DataMath();
+
+    public static DataMath getInstance()
+    {
+        return ourInstance;
+    }
+
+    public double CalculateMean(int startIndex, int endIndex, ArrayList<RealtimeDataSample> dataSet)
+    {
+        int dataSum = 0;
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            dataSum += dataSet.get(i).m_PPG;
+        }
+        return dataSum / (endIndex - startIndex);
+    }
+
+    public double CalculateStdev(int startIndex, int endIndex, ArrayList<RealtimeDataSample> dataSet)
+    {
+        double diffSumSquared = 0.0;
+        double mean = CalculateMean(startIndex, endIndex, dataSet);
+
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            diffSumSquared += Math.pow((dataSet.get(i).m_PPG - mean), 2);
+        }
+        return Math.sqrt(diffSumSquared / (endIndex - startIndex));
+    }
+}
