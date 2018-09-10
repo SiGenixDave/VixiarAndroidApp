ef345befd210b6dedd4b83bcd7f6b632f4e6bbd9
Greg Yurko <gyurko@sigenix.com>
2018-09-10 15:16:15 -0400

    Working on video player.

--- app/src/main/java/com/vixiar/indicor/Activities/VideoActivity.java
+++ app/src/main/java/com/vixiar/indicor/Activities/VideoActivity.java
@@ -1,6 +1,7 @@
 package com.vixiar.indicor.Activities;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
@@ -15,6 +16,7 @@ public class VideoActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_video);
 
         VideoView v = findViewById(R.id.videoView);
@@ -22,8 +24,8 @@ public class VideoActivity extends Activity
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(v);
         v.setMediaController(mediaController);
-        String path = "android.resource://" + getPackageName() + "/" + R.raw.vixvideo;
-        v.setVideoURI(Uri.parse(path));
-        v.start();
+        //String path = "android.resource://" + getPackageName() + "/" + R.raw.vixvideo;
+        //v.setVideoURI(Uri.parse(path));
+        //v.start();
     }
 }
