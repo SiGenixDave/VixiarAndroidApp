99398bbe713eab9d46cd32e5abc8a7eec5f9d3db
Greg Yurko <gyurko@sigenix.com>
2018-10-22 12:26:08 -0400

    Added more information to to the bugsnag report.

--- app/src/main/java/com/vixiar/indicor2/Application/NavigatorApplication.java
+++ app/src/main/java/com/vixiar/indicor2/Application/NavigatorApplication.java
@@ -2,8 +2,13 @@ package com.vixiar.indicor2.Application;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
+import com.bugsnag.android.BeforeNotify;
 import com.bugsnag.android.Bugsnag;
+import com.bugsnag.android.Error;
+import com.vixiar.indicor2.Data.PatientInfo;
 import com.vixiar.indicor2.Upload_Interface.UploadServiceInterface;
 
 /**
@@ -14,14 +19,48 @@ public class NavigatorApplication extends Application
 {
     private static Context m_context;
 
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
         Bugsnag.init(this);
+        Bugsnag.beforeNotify(new BeforeNotify()
+        {
+            @Override
+            public boolean run(Error error)
+            {
+                if (PatientInfo.getInstance().get_handheldSerialNumber() != null)
+                {
+                    error.addToTab("user", "Handheld SN", PatientInfo.getInstance().get_handheldSerialNumber());
+                }
+                else
+                {
+                    error.addToTab("user","Handheld SN", "N/A");
+                }
+
+                if (PatientInfo.getInstance().get_patientId() != null)
+                {
+                    error.addToTab("user", "Patient ID", PatientInfo.getInstance().get_patientId());
+                }
+                else
+                {
+                    error.addToTab("user", "Patient ID", "NA");
+                }
+
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(NavigatorApplication.getAppContext());
+                String subFolder = sp.getString("study_location", "Vixiar_Internal-Testing");
+                error.addToTab("user", "Study Location", subFolder);
+
+                return true;
+            }
+        });
+
         m_context = getApplicationContext();
 
         UploadServiceInterface.getInstance().initialize(this);
     }
 
-    public static Context getAppContext() {
+    public static Context getAppContext()
+    {
         return m_context;
-    }}
+    }
+}
