d170bbbd96f6e191bbbe552dd65317d3f327a946
Greg Yurko <gyurko@sigenix.com>
2019-02-27 09:43:11 -0500

    Renamed some constants.

--- app/src/main/java/com/vixiar/indicor2/Data/AppConstants.java
+++ app/src/main/java/com/vixiar/indicor2/Data/AppConstants.java
@@ -12,11 +12,11 @@ public class AppConstants
     public static final int MAX_STABLE_HR = 120;
     public static final int LOOKBACK_SECONDS_FOR_FLATLINE = 2;
     public static final int LOOKBACK_SECONDS_FOR_MOVEMENT = 2;
-    public static final double  SD_LIMIT_FOR_FLATLINE = 200.0;
+    public static final double PPG_LIMIT_FOR_FLATLINE = 200.0;
     public static final double HF_NOISE_LIMIT = 0.4;
     public static final int SECONDS_BEFORE_T0_FOR_RESULTS_GRAPH = 20;
     public static final int LENGTH_OF_RESULTS_GRAPH = 50;
     public static final int ZERO_CROSSING_IN_MOVEMENT_TIME_LIMIT = 14;
-    public static final double STDEVS_ABOVE_MEAN_LIMIT_FOR_MOVEMENT = 3.5;
-    public static final double STDEVS_BELOW_MEAN_LIMIT_FOR_MOVEMENT = 2.5;
+    public static final double STD_DEVS_ABOVE_MEAN_LIMIT_FOR_MOVEMENT = 3.5;
+    public static final double STD_DEVS_BELOW_MEAN_LIMIT_FOR_MOVEMENT = 2.5;
 }
